840a973f3f0a5c0e9ef07075777de642ac06f9e8
Anjali <anjalidabas1999@gmail.com>
2019-09-27 16:45:59 +0530

    Two Acivity link done

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mStartBtnn: TextView = findViewById(R.id.chty)
+
+        mStartBtnn.setOnClickListener {
+
+            val intent1 = Intent(this, MyActivity1::class.java)
+            startActivity(intent1)
+        }
     }
 }
--- app/src/main/java/com/example/helloworld/MyActivity1.kt
+++ app/src/main/java/com/example/helloworld/MyActivity1.kt
@@ -1,12 +1,22 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MyActivity1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my1)
+
+        val mStartBtn : Button = findViewById(R.id.startbtn)
+
+        mStartBtn.setOnClickListener {
+
+            val intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
